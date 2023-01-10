dbb5bf9e43fbbb220022653f0373f7a1849c5128
Wisit W <wisit@gipsic.com>
2023-01-10 11:33:15 +0700

    move files

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static("game"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
